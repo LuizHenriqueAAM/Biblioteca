d72c698edd1bf06e94e937f1893bf69bcea66611
LuizHenriqueAAM <alvesluizhenrique3@gmail.com>
2025-03-10 10:22:14 -0300

    Continuar Fazendo Reps

--- Repository/LivroRepository.cs
+++ Repository/LivroRepository.cs
@@ -1,12 +1,58 @@
 namespace Biblioteca;
 
-public class LivroRepository{
+public class LivroRepository
+{
     public static void CriarLivro(Livro v)
+    {
+        using (var context = new DbConnection())
         {
-            using (var context = new DbConnection())
+            context.Livro.Add(v);
+            context.SaveChanges();
+        }
+
+
+    }
+    public static List<Livro> PesquisarLivroPorTitulo(string tituloLivro)
+    {
+        using (var context = new DbConnection())
+        {
+            return context.Livro.Where((l) => l.Titulo == tituloLivro).ToList();
+        }
+    }
+
+    public static Livro PesquisarLivroPorId(int Id)
+    {
+        using (var context = new DbConnection())
+        {
+            return context.Livro.Find(Id)!;
+        }
+    }
+
+    public static void AtualizarLivro(Livro novoLivro)
+    {
+        using (var context = new DbConnection())
+        {
+            Livro? antigo = context.Livro.Find(novoLivro.Id);
+            if (antigo != null)
             {
-                context.Livro.Add(v);
+                Console.WriteLine("ok");
+                antigo.Titulo = novoLivro.Titulo;
+                antigo.ISBN = novoLivro.ISBN;
+                context.Livro.Update(antigo);
                 context.SaveChanges();
             }
         }
+    }
+    public static void DeleteLivro(Livro livro)
+    {
+        using (var context = new DbConnection())
+        {
+            Livro? Alvo = context.Livro.Find(livro.Id);
+            if (Alvo != null)
+            {
+                context.Livro.Remove(Alvo);
+                context.SaveChanges();
+            }
+        }
+    }
 }
